71d72d914466898737d7e0c52b3bdf7e03b42a7b
Vitaliy Roy <vitaliybrandet@gmail.com>
2023-04-23 13:39:28 +0300

    Добавьте файлы проекта.

---
+++ lr23/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace lr23
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonDrawOnClick = new System.Windows.Forms.Button();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.labelError = new System.Windows.Forms.Label();
+            this.textBoxT = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ButtonDrawOnClick
+            // 
+            this.ButtonDrawOnClick.Location = new System.Drawing.Point(104, 99);
+            this.ButtonDrawOnClick.Name = "ButtonDrawOnClick";
+            this.ButtonDrawOnClick.Size = new System.Drawing.Size(75, 23);
+            this.ButtonDrawOnClick.TabIndex = 0;
+            this.ButtonDrawOnClick.Text = "ОК";
+            this.ButtonDrawOnClick.UseVisualStyleBackColor = true;
+            this.ButtonDrawOnClick.Click += new System.EventHandler(this.ButtonDrawOnClick_Click);
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(104, 47);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(100, 20);
+            this.textBoxA.TabIndex = 1;
+            // 
+            // labelError
+            // 
+            this.labelError.AutoSize = true;
+            this.labelError.Location = new System.Drawing.Point(346, 207);
+            this.labelError.Name = "labelError";
+            this.labelError.Size = new System.Drawing.Size(0, 13);
+            this.labelError.TabIndex = 2;
+            // 
+            // textBoxT
+            // 
+            this.textBoxT.Location = new System.Drawing.Point(104, 73);
+            this.textBoxT.Name = "textBoxT";
+            this.textBoxT.Size = new System.Drawing.Size(100, 20);
+            this.textBoxT.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(210, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(210, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "t";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxT);
+            this.Controls.Add(this.labelError);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.ButtonDrawOnClick);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonDrawOnClick;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.Label labelError;
+        private System.Windows.Forms.TextBox textBoxT;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ lr23/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lr23
+{
+    public partial class Form1 : Form
+    {
+        private const int Width = 800;
+        private const int Height = 600;
+        private const int Margin = 50;
+        private const float Step = 0.1f;
+
+        private readonly Pen _pen = new Pen(Color.Blue, 2f);
+        private float _a;
+        private float _t;
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetClientSizeCore(Width, Height);
+            DoubleBuffered = true;
+        }
+
+        private void ButtonDrawOnClick_Click(object sender, EventArgs e)
+        {
+            if (float.TryParse(textBoxA.Text, out var a) && float.TryParse(textBoxT.Text, out var t))
+            {
+                _a = a;
+                _t = t;
+                labelError.Visible = false;
+                Invalidate();
+            }
+            else
+            {
+                labelError.Visible = true;
+                labelError.Text = "Невірне значення";
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            var g = e.Graphics;
+
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            g.Clear(BackColor);
+
+            // Зображення осей x та y
+            var pen = new Pen(Color.Black, 2f);
+            g.DrawLine(pen, Margin, Height - Margin, Width - Margin, Height - Margin); // Вісь x
+            g.DrawLine(pen, Margin, Height - Margin, Margin, Margin); // Вісь y
+
+            // Зображення міток x та y
+            var font = new Font("Arial", 12);
+            var brush = Brushes.Black;
+
+            for (int i = Margin + 100; i < Width - Margin; i += 100)
+            {
+                g.DrawString((i - Width / 2).ToString(), font, brush, i, Height - Margin + 5);
+                g.DrawLine(pen, i, Height - Margin - 5, i, Height - Margin + 5);
+            }
+
+            for (int i = Margin + 100; i < Height - Margin; i += 100)
+            {
+                g.DrawString((Height - i - Margin).ToString(), font, brush, Margin - 20, i);
+                g.DrawLine(pen, Margin - 5, i, Margin + 5, i);
+            }
+
+            // Зображення графіка
+            var points = new PointF[Width];
+            for (var i = 0; i < Width; i++)
+            {
+                var currentT = i * Step + _t;
+                var x = _a * (currentT - (float)Math.Pow(Math.Sin(currentT), 2));
+                var y = _a * (currentT - (float)Math.Pow(Math.Cos(currentT), 2));
+                points[i] = new PointF(x + Width / 2, Height - y - Margin);
+            }
+            g.DrawLines(_pen, points);
+        }
+    }
+}
